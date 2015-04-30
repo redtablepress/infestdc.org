a17841b17d96e6ad4779a78c9f08e1e085413a7c
Xaq Rothman <xaq@radcampaign.com>
2015-04-30 12:57:53 -0400

    Add mixtape embed.

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 		
 		<!-- BEGINNING OF INDEX.PHP FILE -->
 		
+    <div class="two-thirds column">
+    <iframe width="100%" height="600" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/77911356&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+</div>
+
 		<div class="one-third column">
 			<a href="http://facebook.com/infestdc"><img class="scale-with-grid" src="images/nav_facebook.png"/></a>
 		</div>
