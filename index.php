78314d6053618f9e65783f053b05139248feac9a
Xaq Rothman <xaq@radcampaign.com>
2015-04-30 14:02:56 -0400

    Add zine header and remove schedule link.

--- index.php
+++ index.php
@@ -2,53 +2,56 @@
 		
 		<!-- BEGINNING OF INDEX.PHP FILE -->
 		
-    <div class="two-thirds column">
-    <h2>Stream the In It Together Fest mixtape!</h2>
-    <p>Free download and tapes for sale <a href="https://dztapes.bandcamp.com/album/infestdc">on Bandcamp</a>.</p>
-    <iframe width="100%" height="600" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/77911356&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-</div>
+    <div class="row">
+      <div class="two-thirds column">
+        <h2>Stream the In It Together Fest mixtape!</h2>
+        <p>Free download and tapes for sale <a href="https://dztapes.bandcamp.com/album/infestdc">on Bandcamp</a>.</p>
+        <iframe width="100%" height="600" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/77911356&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+      </div>
 
-		<div class="one-third column">
-			<a href="http://facebook.com/infestdc"><img class="scale-with-grid" src="images/nav_facebook.png"/></a>
-		</div>
-		
-		<div class="one-third column">
-			<a href="/info.php"><img class="scale-with-grid" src="images/nav_info.png"></a>
-		</div>
-		<div class="one-third column">
-			<a href="mailto:festinyrbasement@gmail.com"><img class="scale-with-grid" src="images/nav_inquiries.png"/></a>
-		</div>
-		<div class="one-third column">
-			<a href="/schedule.php"><img class="scale-with-grid" src="images/nav_schedule.png"/></a>
-		</div>
-		
-		<div class="one-third column" style="background: black; postion: center;">
-		<center><br><img src="images/mailinglist.png"/>
+      <div class="one-third column">
+        <a href="http://facebook.com/infestdc"><img class="scale-with-grid" src="images/nav_facebook.png"/></a>
+      </div>
 
-		<!-- Begin MailChimp Signup Form -->
-		<div id="mc_embed_signup">
-			<form action="http://redtablepress.us4.list-manage.com/subscribe/post?u=75d3eea8f09d43ad443dc49f4&amp;id=fbb0902d2d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-	
-			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-			<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-			<div style="position: absolute; left: -5000px;"><input type="text" name="b_75d3eea8f09d43ad443dc49f4_fbb0902d2d" value=""></div>
-			<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-			</form>
-		</div>
-		<!--End mc_embed_signup-->
-		</center></div>
-		
-		
-		<div class="one-third column">
-			<a href="/twitter.php"><img class="scale-with-grid" src="images/nav_twitter.png"/></a>
-		</div>
-		
-		<div class="one-third column">
-			<img class="scale-with-grid" src="images/spacer.png"/>
-		</div>
-		<div class="sixteen columns">
-			<a href="zine/inFest_zine_vol4retro_WEB.pdf"><img class="scale-with-grid" src="images/zine-tease-cover.png"/></a>
-		</div>
+      <div class="one-third column">
+        <a href="/twitter.php"><img class="scale-with-grid" src="images/nav_twitter.png"/></a>
+      </div>
+
+      <div class="one-third column">
+        <a href="/info.php"><img class="scale-with-grid" src="images/nav_info.png"></a>
+      </div>
+      <div class="one-third column">
+        <a href="mailto:festinyrbasement@gmail.com"><img class="scale-with-grid" src="images/nav_inquiries.png"/></a>
+      </div>
+      <div class="one-third column" style="background: black; postion: center;">
+      <center><br><img src="images/mailinglist.png"/>
+
+      <!-- Begin MailChimp Signup Form -->
+      <div id="mc_embed_signup">
+        <form action="http://redtablepress.us4.list-manage.com/subscribe/post?u=75d3eea8f09d43ad443dc49f4&amp;id=fbb0902d2d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+
+        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;"><input type="text" name="b_75d3eea8f09d43ad443dc49f4_fbb0902d2d" value=""></div>
+        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </form>
+      </div>
+      <!--End mc_embed_signup-->
+      </center></div>
+
+
+      <div class="one-third column">
+        <img class="scale-with-grid" src="images/spacer.png"/>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="sixteen columns">
+        <h2>Check out the 2014 Retrospective zine!</h2>
+        <a href="zine/inFest_zine_vol4retro_WEB.pdf"><img class="scale-with-grid" src="images/zine-tease-cover.png"/></a>
+      </div>
+    </div>
 		
 
 		
