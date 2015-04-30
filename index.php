37ca10a26300bd5f8abcaad206b5f01cd08d07a8
Xaq Rothman <xaq@radcampaign.com>
2015-04-30 13:46:15 -0400

    Add mixtape header and Bandcamp link, with some text styles.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 		<!-- BEGINNING OF INDEX.PHP FILE -->
 		
     <div class="two-thirds column">
+    <h2>Stream the In It Together Fest mixtape!</h2>
+    <p>Free download and tapes for sale <a href="https://dztapes.bandcamp.com/album/infestdc">on Bandcamp</a>.</p>
     <iframe width="100%" height="600" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/77911356&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
 </div>
 
